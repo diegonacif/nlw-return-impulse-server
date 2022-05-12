7b0ec73dc870dae62cbf9b72b74396e082c6bcd8
diegonacif <diegoreisnacif@gmail.com>
2022-05-11 22:02:18 -0300

    iniciando deploy server

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "d5086f444dedb4",
+    pass: "7327f56c6b6c65"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Diego Nacif <diego_nacif@hotmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
